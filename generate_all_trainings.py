46fd88e8b3fe63cc151a93c23c5e343709bebb97
jqueguiner <jean-louis.queguiner@gadz.org>
2021-03-23 15:07:54 +0000

    fix generate all training

--- generate_all_trainings.py
+++ generate_all_trainings.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
-
-
 import os
 import csv
-
-
-# In[20]:
-
 
 with open('wav2vec_languages.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -17,20 +10,11 @@ with open('wav2vec_languages.csv') as csv_file:
     next(csv_reader)
     for (language_code, language_full_name) in csv_reader:
         print(f"#Launching Training for {language_code}-{language_full_name}")
-        cmd = f"ovhai job run --gpu 1 --name '{language_code}-{language_full_name}' --volume output_models@GRA/{language_code}:/workspace/output_models:RW:cache -e model_name_or_path='facebook/wav2vec2-large-xlsr-53' -e dataset_config_name={language_code} -e output_dir='/workspace/output_models/wav2vec2-large-xlsr-{language_code}-{language_full_name}-demo' -e cache_dir='/workspace/data' -e num_train_epochs=10 databuzzword/hf-wav2vec -- sh /workspace/wav2vec/finetune_with_params.sh"
+        cmd = f"ovhai job run --gpu 1 --name '{language_code}-{language_full_name}' --volume data@GRA/{language_code}:/workspace/data/{language_code}:RW:cache --volume output_models@GRA/{language_code}:/workspace/output_models/{language_code}:RW:cache -e model_name_or_path='facebook/wav2vec2-large-xlsr-53' -e dataset_config_name={language_code} -e output_dir='/workspace/output_models/{language_code}/wav2vec2-large-xlsr-{language_code}-{language_full_name}-demo' -e cache_dir='/workspace/data/{language_code}/data' -e num_train_epochs=10 databuzzword/hf-wav2vec -- sh /workspace/wav2vec/finetune_with_params.sh"
         print(cmd)
-        stream = os.popen(cmd)
-        output = stream.read()
-        output
-
-
-# In[3]:
-
-
-
-
-
-# In[ ]:
+#        stream = os.popen(cmd)
+#        output = stream.read()
+#        output
 
 
 
