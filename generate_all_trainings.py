bb27c74a5983820a838a168c28cb0b9cbb47d41d
jqueguiner <jean-louis.queguiner@gadz.org>
2021-03-23 23:09:03 +0000

    handle bigger cache

--- generate_all_trainings.py
+++ generate_all_trainings.py
@@ -10,7 +10,7 @@ with open('wav2vec_languages.csv') as csv_file:
     next(csv_reader)
     for (language_code, language_full_name) in csv_reader:
         print(f"#Launching Training for {language_code}-{language_full_name}")
-        cmd = f"ovhai job run --gpu 1 --name '{language_code}-{language_full_name}' --volume data@GRA/{language_code}:/workspace/data/{language_code}:RW:cache --volume output_models@GRA/{language_code}:/workspace/output_models/{language_code}:RW:cache -e model_name_or_path='facebook/wav2vec2-large-xlsr-53' -e dataset_config_name={language_code} -e output_dir='/workspace/output_models/{language_code}/wav2vec2-large-xlsr-{language_code}-{language_full_name}-demo' -e cache_dir='/workspace/data/{language_code}/data' -e num_train_epochs=10 databuzzword/hf-wav2vec -- sh /workspace/wav2vec/finetune_with_params.sh"
+        cmd = f"ovhai job run --gpu 1 --name '{language_code}-{language_full_name}' --volume cache@GRA:/workspace/.cache:RW:cache --volume data@GRA/{language_code}:/workspace/data/{language_code}:RW:cache --volume output_models@GRA/{language_code}:/workspace/output_models/{language_code}:RW:cache -e model_name_or_path='facebook/wav2vec2-large-xlsr-53' -e dataset_config_name={language_code} -e output_dir='/workspace/output_models/{language_code}/wav2vec2-large-xlsr-{language_code}-{language_full_name}-demo' -e cache_dir='/workspace/data/{language_code}/data' -e num_train_epochs=10 databuzzword/hf-wav2vec -- sh /workspace/wav2vec/finetune_with_params.sh"
         print(cmd)
 
 ## uncomment here to run all jobs directly
